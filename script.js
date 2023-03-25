eb2cf6666a39cd1f3a32dbc23920e3d4e61a3130
Arham659 <122161432+Arham659@users.noreply.github.com>
2023-03-25 17:07:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+//let x = document.getElementById('map') ;
+let btn = document.getElementById('btn') ;
+let removeBtn = document.getElementById('remove-btn') ;
+let frame = document.getElementById('map') ;
+
+
+btn.addEventListener('click', getLocation) ;
+removeBtn.addEventListener('click', clear) ;
+
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      frame.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position){
+localStorage.setItem("Lat:", position.coords.latitude) 
+localStorage.setItem("Long:", position.coords.longitude) 
+console.log(position.coords.latitude, position.coords.longitude);
+  frame.innerHTML = `
+   
+   <iframe
+  src="https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&z=15&output=embed"
+  width="360"
+  height="270"
+  frameborder="0"
+  style="border:0"
+></iframe>`
+  }
+
+  function clear(){
+   
+ localStorage.removeItem("Lat:") 
+localStorage.removeItem("Long:") 
+  }
